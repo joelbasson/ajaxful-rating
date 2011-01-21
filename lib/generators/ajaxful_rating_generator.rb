62184591e5f7d634a661e2da12d02e15861dd389
joelbasson <joelbasson@me.com>
2011-01-21 22:03:29 +0200

    rails 3

--- lib/generators/ajaxful_rating_generator.rb
+++ lib/generators/ajaxful_rating_generator.rb
@@ -1,18 +1,12 @@
 class AjaxfulRatingGenerator < Rails::Generators::NamedBase
+  source_root File.expand_path('../templates', __FILE__)
   
-  def manifest
-    record do |m|
-      m.class_collisions 'Rate'
-      m.template 'model.rb', File.join('app/models', 'rate.rb')
-      m.migration_template 'migration.rb', 'db/migrate',
-        :migration_file_name => 'create_rates'
-      
-      # style
-      m.directory 'public/images/ajaxful_rating'
-      m.file 'images/star.png', 'public/images/ajaxful_rating/star.png'
-      m.file 'images/star_small.png', 'public/images/ajaxful_rating/star_small.png'
-      m.file 'style.css', 'public/stylesheets/ajaxful_rating.css'
-    end
+  def install_files
+    copy_file 'model.rb', 'app/models/rate.rb'
+    copy_file 'migration.rb', 'db/migrate/create_rates.rb'
+    copy_file 'images/star.png', 'public/images/ajaxful_rating/star.png'
+    copy_file 'images/star_small.png', 'public/images/ajaxful_rating/star_small.png'
+    copy_file 'style.css', 'public/stylesheets/ajaxful_rating.css'
   end
   
   protected
