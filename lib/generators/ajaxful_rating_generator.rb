5553943b72ec4a5ee7795354481ba7f6236f5140
joelbasson <joelbasson@me.com>
2011-01-22 01:50:00 +0200

    rails 3

--- lib/generators/ajaxful_rating_generator.rb
+++ lib/generators/ajaxful_rating_generator.rb
@@ -1,6 +1,15 @@
 class AjaxfulRatingGenerator < Rails::Generators::NamedBase
+  include Rails::Generators::Migration
   source_root File.expand_path('../templates', __FILE__)
-  
+    
+  def self.next_migration_number(dirname)
+     if ActiveRecord::Base.timestamped_migrations
+       Time.now.utc.strftime("%Y%m%d%H%M%S")
+     else
+       "%.3d" % (current_migration_number(dirname) + 1)
+     end
+   end
+     
   def install_files
     template 'model.rb', 'app/models/rate.rb'
     migration_template 'migration.rb', 'db/migrate/create_rates.rb'
