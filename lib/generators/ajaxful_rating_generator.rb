aa0f4c38cc82ff65575e3e41b79392d90a1bf933
joelbasson <joelbasson@me.com>
2011-01-22 01:40:30 +0200

    rails 3

--- lib/generators/ajaxful_rating_generator.rb
+++ lib/generators/ajaxful_rating_generator.rb
@@ -2,7 +2,7 @@ class AjaxfulRatingGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   
   def install_files
-    copy_file 'model.rb', 'app/models/rate.rb'
+    template 'model.rb', 'app/models/rate.rb'
     copy_file 'migration.rb', 'db/migrate/create_rates.rb'
     copy_file 'images/star.png', 'public/images/ajaxful_rating/star.png'
     copy_file 'images/star_small.png', 'public/images/ajaxful_rating/star_small.png'
