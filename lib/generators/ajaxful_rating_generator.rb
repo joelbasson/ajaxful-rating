b4a412ec33641cb9c8adbe402191737413d98639
joelbasson <joelbasson@me.com>
2011-01-22 01:35:31 +0200

    rails 3

--- lib/generators/ajaxful_rating_generator.rb
+++ lib/generators/ajaxful_rating_generator.rb
@@ -1,19 +1,13 @@
-class AjaxfulRatingGenerator < Rails::Generator::NamedBase
+class AjaxfulRatingGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   
-  def manifest
-      record do |m|
-        m.class_collisions 'Rate'
-        m.template 'model.rb', File.join('app/models', 'rate.rb')
-        m.migration_template 'migration.rb', 'db/migrate',
-          :migration_file_name => 'create_rates'
-
-        # style
-        m.file 'images/star.png', 'public/images/ajaxful_rating/star.png'
-        m.file 'images/star_small.png', 'public/images/ajaxful_rating/star_small.png'
-        m.file 'style.css', 'public/stylesheets/ajaxful_rating.css'
-      end
-    end
+  def install_files
+    copy_file 'model.rb', 'app/models/rate.rb'
+    copy_file 'migration.rb', 'db/migrate/create_rates.rb'
+    copy_file 'images/star.png', 'public/images/ajaxful_rating/star.png'
+    copy_file 'images/star_small.png', 'public/images/ajaxful_rating/star_small.png'
+    copy_file 'style.css', 'public/stylesheets/ajaxful_rating.css'
+  end
   
   protected
   
--- lib/generators/templates/model.rb
+++ lib/generators/templates/model.rb
@@ -1,5 +1,5 @@
 class Rate < ActiveRecord::Base
-  belongs_to :rater, :class_name => "<%= name.classify %>"
+  belongs_to :rater, :class_name => "<%= class_name %>"
   belongs_to :rateable, :polymorphic => true
   
   attr_accessible :rate, :dimension
